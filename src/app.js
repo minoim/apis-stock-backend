270d060961d0f74d8d0e99fd2acb4e61e6d80561
minoim <kimminho@gimminhoui-MacBookPro.local>
2024-12-08 14:27:40 +0900

    Fix CORS configuration

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
+const express = require('express');
 const cors = require('cors');
+const app = express();
 
-// CORS 설정을 더 유연하게 수정
-app.use(cors({
-  origin: [
-    'https://www.apis-stock.com',
-    'https://apis-stock.com',
-    'http://localhost:3000'  // 개발 환경용
-  ],
-  credentials: true,
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-})); 
+// CORS 미들웨어를 가장 앞에 배치
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://www.apis-stock.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  next();
+});
+
+// 기존의 cors() 미들웨어는 제거
+// app.use(cors({ ... }));
+
+// 나머지 미들웨어와 라우트 설정 
