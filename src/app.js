20776417bdfbf364a82d5aabc162657c3c5f567c
minoim <kimminho@gimminhoui-MacBookPro.local>
2024-12-08 14:25:01 +0900

    Update CORS configuration

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
-const express = require('express');
 const cors = require('cors');
-const app = express();
 
+// CORS 설정을 더 유연하게 수정
 app.use(cors({
-  origin: 'https://www.apis-stock.com',
-  credentials: true
+  origin: [
+    'https://www.apis-stock.com',
+    'https://apis-stock.com',
+    'http://localhost:3000'  // 개발 환경용
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 })); 
