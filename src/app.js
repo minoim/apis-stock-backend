36f3eaa012cc02e382c33ed599a1cfbcc18233ca
minoim <kimminho@gimminhoui-MacBookPro.local>
2024-12-08 14:31:01 +0900

    Update CORS configuration to allow all origins

--- src/app.js
+++ src/app.js
@@ -2,21 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// CORS 미들웨어를 가장 앞에 배치
+// 모든 요청에 대해 CORS 허용
+app.use(cors());
+
+// 추가 CORS 헤더 설정
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://www.apis-stock.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-  
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
-
-// 기존의 cors() 미들웨어는 제거
-// app.use(cors({ ... }));
 
 // 나머지 미들웨어와 라우트 설정 
